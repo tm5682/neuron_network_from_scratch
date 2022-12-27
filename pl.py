2f2d3d561d77cbd34fcf2437d83d9f67cd624dd4
Tahrin Maruf <tm5682@mun.ca>
2022-12-27 02:10:38 -0330

    activation function understanding

--- pl.py
+++ pl.py
@@ -1,6 +1,5 @@
 # import sys
-import numpy as np
-import matplotlib
+
 
 # print ("Python:" , sys.version)
 # print("Numpy:", np.__version__)
@@ -29,11 +28,24 @@ import matplotlib
 # print (output)
 
 
+import numpy as np
+import matplotlib
+
+import nnfs
+from nnfs.datasets import spiral_data
+
+nnfs.init()
+
+
+
 np.random.seed(0)
 
-X =    [[1, 2, 3, 2.5 ],
-        [2.0, 5.0, -1.0, 2.0],
-        [-1.5, 2.7, 3.3, -0.8]]
+# X =    [[1, 2, 3, 2.5 ],
+#         [2.0, 5.0, -1.0, 2.0],
+#         [-1.5, 2.7, 3.3, -0.8]]
+
+X, y = spiral_data (100, 3)
+
 
 class Layer_Dense:
     def __init__ (self, n_inputs, n_neurons):
@@ -43,10 +55,19 @@ class Layer_Dense:
         self.output = np.dot(inputs, self.weights) + self.biases
 
 
-layer1 = Layer_Dense(4,5)
-layer2 = Layer_Dense(5,2)
+class Actiovation_ReLU:
+    def forward (self, inputs):
+        self.output = np.maximum(0,inputs)
 
-layer1.forward(X)   
-#print(layer1.output)
-layer2.forward(layer1.output)
-print(layer2.output)
+
+layer1 = Layer_Dense(2,5)
+
+activation1 = Actiovation_ReLU()
+
+layer1.forward(X) 
+
+
+activation1.forward(layer1.output)
+
+print (activation1.output)
+
